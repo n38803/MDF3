e69bc2d7da268ae8f7266b3c953ffa0b8344b80d
n38803 <shaunthompson@fullsail.edu>
2015-01-11 20:17:04 -0500

    Save before I break all code.

--- MDF3_W1/app/src/main/java/android/fullsail/com/mdf3_w1/MusicService.java
+++ MDF3_W1/app/src/main/java/android/fullsail/com/mdf3_w1/MusicService.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 /**
  * Created by Shaun on 1/11/2015.
  */
-public class MusicService extends Service  {
+public class MusicService extends Service implements MediaPlayer.OnPreparedListener {
 
         /*
         // MEDIA STATES
@@ -51,19 +51,166 @@ public class MusicService extends Service  {
 
     }
 
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-
-
-        return Service.START_NOT_STICKY;
-    }
-
-
     @Override
     public IBinder onBind(Intent intent) {
         // Don't allow binding.
         return null;
     }
 
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Song track1 = new Song(
+                "Ghost of the Machine",
+                "Dragons Make Bad Babysitters",
+                ("android.resource://" + getPackageName() + "/" + R.raw.gotmdragons)
+        );
+
+        Song track2 = new Song(
+                "Ghost of the Machine",
+                "On Letting Go",
+                ("android.resource://" + getPackageName() + "/" + R.raw.gotmlettinggo)
+        );
+
+        Song track3 = new Song(
+                "Ghost of the Machine",
+                "Baby, There's No Escape Cuz' I Sold the Exit Door",
+                ("android.resource://" + getPackageName() + "/" + R.raw.gotmbaby)
+        );
+
+        Song track4 = new Song(
+                "Ghost of the Machine",
+                "She's Not Dancing, She's Dying!",
+                ("android.resource://" + getPackageName() + "/" + R.raw.gotmdancing)
+        );
+
+
+        Song songs[] = {track1, track2, track3, track4};
+
+        // Assign textviews
+        TextView band = (TextView) v.getRootView().findViewById(R.id.bandName);
+        TextView song = (TextView) v.getRootView().findViewById(R.id.songName);
+
+
+        if(mPlayer == null) {
+            // assign & initiate media player
+            mPlayer = new MediaPlayer();
+            mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mPlayer.setOnPreparedListener(this);
+            songPosition = 0;
+            mIdle = false;
+
+            try {
+                mPlayer.setDataSource(this, Uri.parse(songs[songPosition].getTrack()));
+                mIdle = false;
+                Log.i("Initiating", "Track: " + songPosition);
+            } catch(IOException e) {
+                e.printStackTrace();
+                Log.e("ERROR!", "--PLAYER RELEASED");
+                mPlayer.release();
+                mPlayer = null;
+            }
+        }
+
+        else if (mPlayer != null && mIdle != true)
+        {
+            Log.i("RESUMING", "Idle: " + mIdle);
+            onResume();
+
+        }
+
+        else if (mPlayer != null && mIdle == true){
+            try {
+                mPlayer.setDataSource(this, Uri.parse(songs[songPosition].getTrack()));
+                mIdle = false;
+                onResume();
+            } catch(IOException e) {
+                e.printStackTrace();
+                Log.e("ERROR!", "--PLAYER RELEASED");
+                mPlayer.release();
+                mPlayer = null;
+            }
+        }
+
+
+
+
+        band.setText(songs[songPosition].getArtist());
+        song.setText(songs[songPosition].getSong());
+
+
+        return Service.START_NOT_STICKY;
+    }
+
+    protected void onResume() {
+
+
+
+        mActivityResumed = true;
+
+
+        if(mPlayer != null && !mPrepared) {
+            Log.i("Preparing", "Track: " + songPosition);
+            mPlayer.prepareAsync();
+        } else if(mPlayer != null && mPrepared) {
+            Log.i("Resuming", "Track: " + songPosition);
+            mPlayer.seekTo(mAudioPosition);
+            mPlayer.start();
+        }
+    }
+
+    protected void onPause() {
+        mActivityResumed = false;
+
+        if(mPlayer != null && mPlayer.isPlaying()) {
+            mPlayer.pause();
+            mAudioPosition = mPlayer.getCurrentPosition();
+        }
+    }
+
+    protected void onStop() {
+
+        if(mPlayer != null && mPlayer.isPlaying()) {
+            Log.e("MusicPlayer", "Stopped");
+            mPlayer.stop();
+            mPrepared = false;
+            mAudioPosition = 0;
+
+        }
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+
+        if(mPlayer != null) {
+            mPlayer.release();
+        }
+    }
+
+    protected void onReset() {
+        if(mPlayer != null) {
+
+            Log.e("MusicPlayer", "RESET");
+            mAudioPosition = 0;
+            mPlayer.reset();
+            mPrepared = false;
+            mIdle = true;
+
+
+        }
+    }
+
+
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        mPrepared = true;
+
+        if(mPlayer != null && mActivityResumed) {
+
+            mPlayer.seekTo(mAudioPosition);
+            mPlayer.start();
+        }
+    }
 }
